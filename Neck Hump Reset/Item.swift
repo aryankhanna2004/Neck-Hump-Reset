63e4b9fec222f30420f9c210735064bcb156b520
Aryan Khanna <aryankhanna2004@gmail.com>
2025-12-28 16:08:54 -0700

    Initial Commit

---
+++ Neck Hump Reset/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Neck Hump Reset
+//
+//  Created by ET Loaner on 12/28/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
